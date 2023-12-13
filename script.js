467b9bfe69e7e76d6017c9eba8b0f39efa8572b7
CyrilMougin <c.mougin@hotmail.fr>
2023-12-13 22:08:39 +0100

    hovering effect

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 gridContainer = document.createElement('div');
 gridContainer.classList.add('container')
-gridContainer.style.display = 'flex';
-gridContainer.style.flexWrap = 'wrap';
-gridContainer.style.height = '408x';
-gridContainer.style.width = '408px';
-
-gridContainer.style.backgroundColor ='lightPink'
 document.body.appendChild(gridContainer);
+
 
 for (i=0;i<16;i++) {
     tmp = document.createElement('div');
-    tmp.style.height = '100px';
-    tmp.style.width = '100px';
-    tmp.style.backgroundColor = 'lightGreen'
-    tmp.style.border = '1px solid black';
-
     tmp.classList.add("colorBlock");
+    tmp.addEventListener("mouseover", (e) => e.currentTarget.style.backgroundColor = 'green' )
     gridContainer.appendChild(tmp);
 
 }

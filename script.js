344d4afd82c04428b012d02d29155d86da5c7240
Cyril Mougin <cyril.mougin@lusis.fr>
2023-12-13 12:06:13 +0100

    new grid hardcoded

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 gridContainer = document.createElement('div');
 gridContainer.classList.add('container')
 gridContainer.style.display = 'flex';
+gridContainer.style.flexWrap = 'wrap';
+gridContainer.style.height = '408x';
+gridContainer.style.width = '408px';
+
+gridContainer.style.backgroundColor ='lightPink'
 document.body.appendChild(gridContainer);
 
 for (i=0;i<16;i++) {
     tmp = document.createElement('div');
-    tmp.style.height = '50px';
-    tmp.style.width = '50px';
-    tmp.style.backgroundColor = 'red'
+    tmp.style.height = '100px';
+    tmp.style.width = '100px';
+    tmp.style.backgroundColor = 'lightGreen'
     tmp.style.border = '1px solid black';
 
     tmp.classList.add("colorBlock");

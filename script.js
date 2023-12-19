6869c62a1a38233177f6b5e4b9a4146543fec42c
Cyril Mougin <cyril.mougin@lusis.fr>
2023-12-19 14:48:45 +0100

    working grid

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ gridContainer = document.createElement('div');
 gridContainer.classList.add('container')
 document.body.appendChild(gridContainer);
 
+document.querySelector('#gridSizeButton').addEventListener("click", function() {
+    setGridSize(window.prompt());
+});
 
-for (i=0;i<16;i++) {
-    tmp = document.createElement('div');
-    tmp.classList.add("colorBlock");
-    tmp.addEventListener("mouseover", (e) => e.currentTarget.style.backgroundColor = 'green' )
-    gridContainer.appendChild(tmp);
 
+function setGridSize(value) {
+    removeAllChildNodes(gridContainer);
+    for (i=0;i<value*value;i++) {
+        tmp = document.createElement('div');
+        tmp.classList.add("colorBlock");
+        blockSize = 400 / value + 'px';
+        tmp.style.height = blockSize;
+        tmp.style.width = blockSize;
+        tmp.addEventListener("mouseover", (e) => e.currentTarget.style.backgroundColor = 'lightpink' )
+        gridContainer.appendChild(tmp);
+    }
+}
+
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
